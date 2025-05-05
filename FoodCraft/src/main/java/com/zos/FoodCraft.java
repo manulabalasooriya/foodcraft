e40086ed6a9de6853c04517ba3b44a0adc537d62
iamkumari <navanjana38@gmail.com>
2025-05-05 18:56:06 +0530

    all

---
+++ FoodCraft/src/main/java/com/zos/FoodCraft.java
@@ -0,0 +1,13 @@
+package com.zos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodCraft {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodCraft.class, args);
+	}
+
+}
